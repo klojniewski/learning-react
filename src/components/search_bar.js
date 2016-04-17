98778b167d7cb04a7b472890c0a54c35c58a28e6
Krzysztof Lojniewski <kontakt@pagepro.pl>
2016-04-17 19:27:24 +0200

    1_-_An_Intro_to_React

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+    }
+    onInputChange(e) {
+        this.setState({
+            term: e.target.value
+        });
+    }
+    render () {
+        return (
+            <div>
+                <input 
+                    value={this.state.term} 
+                    onChange={this.onInputChange.bind(this)} />
+            </div>
+        );
+    }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAnRC5JxMJlWf_NcfecSkCX-fdcqzMsIjw';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <h1>ReactApp!</h1>
+            <SearchBar />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
