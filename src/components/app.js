dc20a7498bcefc448c4237cdb6878a0056e4f5e8
Krzysztof Lojniewski <kontakt@pagepro.pl>
2016-05-08 21:21:27 +0200

    4_-_Managing_App_State_with_Redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
+
 class App extends Component {
   render() {
     return (
-      <div>React simple starter, hello!</div>
+        <div>
+          <BookList />
+          <BookDetail />
+        </div>
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'JS: Good Parts', pages: 22},
+        { title: 'Surfing Expert', pages: 122},
+        { title: 'Eloquent JS', pages: 2},
+        { title: 'You dont know JS', pages: 3}
+    ]
+}
